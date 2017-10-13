57c4820f023fb7a4afa81c228424258e119c2782
bradberger01 <bradberger@hotmail.com>
2017-10-12 20:44:47 -0400

    First Commit

---
+++ BookShelf2/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace BookShelf2.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<BookShelf2.Models.BookShelf2Context>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "BookShelf2.Models.BookShelf2Context";
+        }
+
+        protected override void Seed(BookShelf2.Models.BookShelf2Context context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters", AuthorId = 10 },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
